81152eca14839d3580b4b87f7cc6673bbab33e2f
Sneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>
2018-05-01 00:37:11 -0500

    add pro

---
+++ app/src/main/java/com/example/sneider/myapplication/Point.java
@@ -0,0 +1,72 @@
+package com.example.sneider.myapplication;
+
+/*
+ * Portions (c) 2009 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Coby Plain coby.plain@gmail.com, Ali Muzaffar ali@muzaffar.me
+ */
+
+public class Point {
+	public double longitude = 0f;
+	public double latitude = 0f;
+	public String description;
+	public float x, y = 0;
+	
+	public Point(double lat, double lon, String desc) {
+		this.latitude = lat;
+		this.longitude = lon;
+		this.description = desc;
+	}
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+}
